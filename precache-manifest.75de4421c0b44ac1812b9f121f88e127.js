301b32205dfe768fc5cb114a62e7c2f417ce4b58
StephenNoh <Stephen.Noh@gmail.com>
2020-08-12 20:01:33 -0500

    Updates

---
+++ precache-manifest.75de4421c0b44ac1812b9f121f88e127.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4f3f514352501ded273f819b54a2b52",
+    "url": "/BoylenQuoteMachine/index.html"
+  },
+  {
+    "revision": "ccd5f227199610d45860",
+    "url": "/BoylenQuoteMachine/static/css/main.9721610e.chunk.css"
+  },
+  {
+    "revision": "5e3db4a13061c6853916",
+    "url": "/BoylenQuoteMachine/static/js/2.421798e9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/BoylenQuoteMachine/static/js/2.421798e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ccd5f227199610d45860",
+    "url": "/BoylenQuoteMachine/static/js/main.ceebc88d.chunk.js"
+  },
+  {
+    "revision": "1efe6f1dde0dd56ed003",
+    "url": "/BoylenQuoteMachine/static/js/runtime-main.4fd2aab8.js"
+  },
+  {
+    "revision": "23190fa5659d6eeb4ea7008eb4dcccb3",
+    "url": "/BoylenQuoteMachine/static/media/boylencircle.23190fa5.png"
+  }
+]);
